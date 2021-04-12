54c0ac2f8ab96d577c45ee34daf3d00d0e947a42
19jaredbarrett <55817962+19jaredbarrett@users.noreply.github.com>
2021-04-12 14:29:25 -0400

    Floyd Warshall Implementation

--- src/FloydWarshall.java
+++ src/FloydWarshall.java
@@ -1,20 +1,43 @@
 public class FloydWarshall {
 
-    public static void main(String[] args) {
+    public static final int MAX_INT = 100000000;
 
+    public static void main(String[] args) {
+        int graph[][] = {   {0,         5,      MAX_INT,    10},
+                            {MAX_INT,   0,      3,          MAX_INT},
+                            {MAX_INT,   MAX_INT,0,          1},
+                {MAX_INT, MAX_INT, MAX_INT, 0}
+        };
+        for(int[] row : floydWarshall(graph, 4)) {
+            for(int num : row) {
+                if(num == MAX_INT)
+                    System.out.print("NF");
+                else
+                    System.out.print(num);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
     }
 
-    public void FloydWarshall(int graph[][], int numVertices) {
+    public static int[][] floydWarshall(int graph[][], int numVertices) {
         int[][] solution = new int[numVertices][numVertices];
+        // initialize our solution matrix, for pairs of all vertices
+        // if the graph has a null value, the number value is simply Math.Infinity()
+        for(int i = 0; i < numVertices; i++)
+            for(int j = 0; j < numVertices; j++)
+                solution[i][j] = graph[i][j];
 
-        // first, we must initialize the solution graph
-        for(int k = 1; k < numVertices; k++) {
-            for (int i = 1; i < numVertices; i++) {
-                for (int j = 1; j < numVertices; j++) {
-
+        // first, we must initialize the solution matrix
+        for(int k = 0; k < numVertices; k++) {
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
+                    if(solution[i][k] + solution[k][j] < solution[i][j])
+                        solution[i][j] = solution[i][k] + solution[k][j];
                 }
             }
         } // end for loops
 
+        return solution;
     }
 }
